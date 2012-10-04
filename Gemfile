a157d0a23a636bc93a1d2e569b48a5c4b5070fa1
Aaron Crespo <aaroncrespo@gmail.com>
2012-10-04 10:58:07 -0400

    initial commit and gem setup

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'jquery-rails'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+	gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'cucumber-rails', :require => false
+  gem 'database_cleaner'
+  gem 'guard-rspec'
+  gem 'rb-fsevent'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataEngineering::Application.config.secret_token = 'd0cdeaa2206498abc19f97bf79143cbee681704b5e448d8c223b61690f94b3190f123d84c02290d0d51cd988b7b0a93ef6d5518ae10f35f11e26527541822dba'
